b128b8e4084b20e75df8088881a9a6da89ba0279
dphan1 <dphan1@my.okcu.edu>
2013-06-24 13:35:58 -0500

    Added authentication logic

--- ProjectBeaver/app/controllers/BeaverController.php
+++ ProjectBeaver/app/controllers/BeaverController.php
@@ -21,8 +21,23 @@
       public function getLoginfailed() {
          return View::make('loginfailed');
       }
-  
+
       public function getProfile() {
          return View::make('profile');
+      }
+  
+      public function showProfile() {
+         $email = Input::get('email'); // Get email input by user from the login form
+         $password = Input::get('password'); // Get password input by user from the login form
+
+         $resultArray = DB::select("SELECT password FROM users WHERE email = '" . $email . "'");
+         $result = (array) $resultArray[0];
+         $hashedPassword = $result['password']; // Get the hashed password from database
+ 
+         if (Hash::check($password, $hashedPassword)) { // Password matched
+            return Redirect::to('main/profile');
+         } else { // Password not matched 
+            return Redirect::to('main/loginfailed');
+         }
       }
    }
