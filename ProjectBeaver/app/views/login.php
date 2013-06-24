f0194f4a042f035549f93a1be384961f50accc27
dphan1 <dphan1@my.okcu.edu>
2013-06-24 13:38:37 -0500

    Modified the form action so it correctly points to the desired method

--- ProjectBeaver/app/views/login.php
+++ ProjectBeaver/app/views/login.php
@@ -1,11 +1,11 @@
-<?php echo Form::open(array('url' => 'main/authenticate')); ?>
-   <?php echo Form::label('username', 'Username'); ?>
-   <?php echo Form::text('whatup', ''); ?>
+<?php echo Form::open(array('action' => 'BeaverController@showProfile')); ?>
+   <?php echo Form::label('emailLabel', 'Email'); ?>
+   <?php echo Form::text('email', ''); ?>
   
 
-   <?php echo Form::label('password', 'Password') ?> 
+   <?php echo Form::label('passwordLabel', 'Password') ?> 
    <?php echo Form::password('password'); ?>
 
+   <?php echo '<br>'; ?>
    <?php echo Form::submit('Login'); ?>
-
    <?php echo Form::close(); ?>
