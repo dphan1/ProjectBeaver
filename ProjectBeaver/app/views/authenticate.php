7aef0f533af2844cfd79a71a5c52acfd4febb561
dphan1 <dphan1@my.okcu.edu>
2013-06-24 11:05:03 -0500

    Using header()

--- ProjectBeaver/app/views/authenticate.php
+++ ProjectBeaver/app/views/authenticate.php
@@ -1,23 +1,22 @@
 <?php
    $email = Input::get('email');
-   // echo "Your input email is: " . $email . "<br>";
+ 
 
    $password = Input::get('password');
-   // echo "Your input password is: " . $password . "<br>";
-
+  
    $result = DB::select("SELECT password FROM users WHERE email = '" . $email . "'");  
    $hashedPassword = (array) $result[0];
-   // echo $hashedPassword['password'] . "<br>";
+ 
 
    if (Hash::check($password, $hashedPassword['password'])) {
 
       // Authentication successful, routing to profile.php         
-      // echo 'Authentication successful!';
-      Redirect::to('main/profile');
+   
+      header('Location: http://pinleague.frontend.localhost/main/profile');
+
    } else {
 
       // Authentication failed, routing to loginfailed.php      
-      echo 'Authentiation failed!';
+   
    }
-
 ?>
