e6f91093abae2367174035f87ab2324db30ae1bc
dphan1 <dphan1@my.okcu.edu>
2013-06-24 10:48:11 -0500

    Added Redirect::to, did not work

--- ProjectBeaver/app/views/authenticate.php
+++ ProjectBeaver/app/views/authenticate.php
@@ -1,19 +1,19 @@
 <?php
    $email = Input::get('email');
-   echo "Your input email is: " . $email . "<br>";
+   // echo "Your input email is: " . $email . "<br>";
 
    $password = Input::get('password');
-   echo "Your input password is: " . $password . "<br>";
+   // echo "Your input password is: " . $password . "<br>";
 
    $result = DB::select("SELECT password FROM users WHERE email = '" . $email . "'");  
    $hashedPassword = (array) $result[0];
-   echo $hashedPassword['password'] . "<br>";
+   // echo $hashedPassword['password'] . "<br>";
 
    if (Hash::check($password, $hashedPassword['password'])) {
 
       // Authentication successful, routing to profile.php         
-      echo 'Authentication successful!';
-      
+      // echo 'Authentication successful!';
+      Redirect::to('main/profile');
    } else {
 
       // Authentication failed, routing to loginfailed.php      
